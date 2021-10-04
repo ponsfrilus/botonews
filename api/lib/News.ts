8ad67e274740139cf12f4d17c1610ba0eb9b6475
Nicolas Borboën <ponsfrilus@gmail.com>
2021-10-04 17:50:36 +0200

    [refactor] Avoid errors when channels are null

--- api/lib/News.ts
+++ api/lib/News.ts
@@ -3,24 +3,26 @@ import { fetchHackernews } from './FetchHackernews';
 import { fetchGoEpfl } from './FetchGoEpfl';
 
 const news = async (req: Request, res: Response, next: Next) => {
-  console.log(req.query.src);
-  let news: any = [];
-
+  let news: BotonewsItem[] = [];
   if (!req.query.src) {
-    let HackerNews: any[] = await fetchHackernews(req.query);
-
+    let HackerNews: BotonewsItem[] = await fetchHackernews(req.query);
     news = news.concat(HackerNews);
-  }
+  } else {
+    const channels = req.query.src.split(',');
+    console.log(channels);
 
-  if (req.query.src.includes('hackernews')) {
-    let hackerNews: any[] = await fetchHackernews(req.query);
-    news = news.concat(hackerNews);
-  }
-  if (req.query.src.includes('goepfl')) {
-    let goEpfl: any[] = await fetchGoEpfl(req.query);
-    news = news.concat(goEpfl);
-  }
+    if (channels.includes('hackernews')) {
+      console.debug('  ↳ adding hackernews');
+      let hackerNews: BotonewsItem[] = await fetchHackernews(req.query);
+      news = news.concat(hackerNews);
+    }
 
+    if (channels.includes('goepfl')) {
+      console.debug('  ↳ adding goepfl');
+      let goEpfl: BotonewsItem[] = await fetchGoEpfl(req.query);
+      news = news.concat(goEpfl);
+    }
+  }
   res.send(news);
   next();
 };
