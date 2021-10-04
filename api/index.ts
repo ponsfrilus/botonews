5569f16d2c66981c52545b65bac8f1f3e72cb2a9
Sami Panseri <sami.panseri@epfl.ch>
2021-10-04 14:03:23 +0200

    [Janitor] Cleanup

--- api/index.ts
+++ api/index.ts
@@ -8,12 +8,6 @@ server.use(restify.plugins.queryParser());
 var apiRoutes = new ApiRoutes(server);
 apiRoutes.listen();
 
-var test: BotonewsItem = {
-  name: "test",
-  title: "test",
-  image_url: new URL("https://www.google.com"),
-};
-
 server.listen(port, function () {
   console.log("%s listening at %s", server.name, server.url);
 });
