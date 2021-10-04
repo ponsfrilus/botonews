d5a5bcb263ecf44ee4dbe162347fae00a6651421
Sami Panseri <sami.panseri@epfl.ch>
2021-10-04 16:30:33 +0200

    [feat] Can handle multiple channels query string

--- api/lib/News.ts
+++ api/lib/News.ts
@@ -1,9 +1,22 @@
 import { Next, Request, Response } from "restify";
-import { fetchHackernews } from "./FetchHackernews"
+import { fetchHackernews } from "./FetchHackernews";
 
 const news = async (req: Request, res: Response, next: Next) => {
-  let HackerNews:any[] = await fetchHackernews(req.query)
-  res.send(HackerNews);
+  console.log(req.query.src);
+  let news: any = [];
+
+  if (!req.query.src) {
+    let HackerNews: any[] = await fetchHackernews(req.query);
+
+    news = news.concat(HackerNews);
+  }
+
+  if (req.query.src.includes("hackernews")) {
+    let hackerNews: any[] = await fetchHackernews(req.query);
+    news = news.concat(hackerNews);
+  }
+
+  res.send(news);
   next();
 };
 
