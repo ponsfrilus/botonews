fa8da7d622e0c450001fa58f43d8679b15f5c8a5
Sami Panseri <sami.panseri@epfl.ch>
2021-10-05 07:43:19 +0200

    [feat] Added actu EPFL channel

---
+++ api/lib/FetchActu.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+export const fetchActu = async (options = {}) => {
+  let defaultoptions = {
+    lang: 'en',
+    number: 3, // Max is 100
+  };
+  let opt = { ...defaultoptions, ...options };
+
+  let articles = [];
+
+  const response = await axios.get(
+    `https://actu.epfl.ch/api/v1/channels/1/news/?lang=${opt.lang}&limit=${opt.number}`
+  );
+  const respdata: any = response.data;
+
+  console.log(respdata);
+
+  for (let i = 0; i != opt.number; i++) {
+    let article: BotonewsItem = {
+      src_channel: 'Actu EPFL',
+      title: respdata.results[i].title,
+      subtitle: respdata.results[i].subtitle,
+      item_url: respdata.results[i].news_url,
+      image_url: new URL(respdata.results[i].thumbnail_url),
+      image_alt: respdata.results[i].visual_description,
+      created_at: respdata.results[i].publish_date,
+    };
+    articles.push(article);
+  }
+  return articles;
+};
--- api/lib/News.ts
+++ api/lib/News.ts
@@ -1,14 +1,16 @@
 import { Next, Request, Response } from 'restify';
 import { fetchHackernews } from './FetchHackernews';
 import { fetchGoEpfl } from './FetchGoEpfl';
+import { fetchActu } from './FetchActu';
 
 const news = async (req: Request, res: Response, next: Next) => {
   let news: BotonewsItem[] = [];
   if (!req.query.src) {
     let hackerNews: BotonewsItem[] = await fetchHackernews(req.query);
     let goEpfl: BotonewsItem[] = await fetchGoEpfl(req.query);
+    let actus: BotonewsItem[] = await fetchActu(req.query);
 
-    news = news.concat(hackerNews, goEpfl);
+    news = news.concat(hackerNews, goEpfl, actus);
   } else {
     const channels = req.query.src.split(',');
     console.log(channels);
@@ -23,6 +25,12 @@ const news = async (req: Request, res: Response, next: Next) => {
       console.debug('  ↳ adding goepfl');
       let goEpfl: BotonewsItem[] = await fetchGoEpfl(req.query);
       news = news.concat(goEpfl);
+    }
+
+    if (channels.includes('actu')) {
+      console.debug(' ↳ adding actu');
+      let actus: BotonewsItem[] = await fetchActu(req.query);
+      news = news.concat(actus);
     }
   }
   res.send(news);
