bffd6d7ea5fd9ac87675d38fc9369986375f7880
Azecko <sami.panseri@epfl.ch>
2021-11-26 11:11:40 +0100

    [feat] Welcome express goodbye restify

--- api/lib/user.ts
+++ api/lib/user.ts
@@ -1,10 +1,10 @@
-import { Next, Request, Response } from 'restify';
+import { NextFunction, Request, Response } from 'express';
 import mysql from 'mysql2';
 import * as dotenv from 'dotenv';
 dotenv.config({ path: __dirname + '/.env' });
 var hash = require('blueimp-md5');
 
-const user = async (req: Request, res: Response, next: Next) => {
+const user = async (req: Request, res: Response, next: NextFunction) => {
   const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -19,14 +19,14 @@ const user = async (req: Request, res: Response, next: Next) => {
             "status_code": 400,\
             "error": "Please specify a username or an id."\
         }';
-      if (!req.query.username && !req.query.id) return res.send(400, JSON.parse(error));
+      if (!req.query.username && !req.query.id) return res.status(400) && res.send(JSON.parse(error));
 
       var where;
       var param;
 
       if (req.query.id) {
         where = 'user';
-        param = parseInt(req.query.id);
+        param = parseInt(req.query.id as string);
       }
       if (req.query.username) {
         where = 'username';
@@ -49,7 +49,7 @@ const user = async (req: Request, res: Response, next: Next) => {
             "error": "Please specify a username and email."\
         }';
       if (!req.body || !req.body.username || !req.body.email) {
-        return res.send(400, JSON.parse(error));
+        return res.status(400) && res.send(JSON.parse(error));
       }
       let hashedEmail = hash(req.body.email);
       db.execute(
@@ -57,7 +57,8 @@ const user = async (req: Request, res: Response, next: Next) => {
         [req.body.username, req.body.email],
         async function (err, results, fields) {
           if (err) return res.send(err);
-          res.send(201, results);
+          res.status(201);
+          res.send(results);
           next();
         }
       );
@@ -68,7 +69,7 @@ const user = async (req: Request, res: Response, next: Next) => {
             "status_code": 400,\
             "error": "Please specify a username."\
         }';
-      if (!req.body.username) return res.send(400, JSON.parse(error));
+      if (!req.body.username) return res.status(400) && res.send(JSON.parse(error));
       db.execute(
         `DELETE FROM t_users WHERE username = ?`,
         [req.body.username],
