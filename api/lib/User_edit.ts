ab436fa2e00021af573ed2fad34493b2767f0b91
Azecko <sami.panseri@epfl.ch>
2022-01-21 08:38:30 +0100

    [fix] user_edit response

--- api/lib/User_edit.ts
+++ api/lib/User_edit.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { getUserByEmail, updateUser } from './DB';
-const user_edit = async (req: Request, res: Response, next: NextFunction) => {
+const user_edit = async (req: any, res: Response, next: NextFunction) => {
   switch (req.method) {
     case 'POST':
       var error =
@@ -17,8 +17,17 @@ const user_edit = async (req: Request, res: Response, next: NextFunction) => {
         return res.status(404) && res.send(JSON.parse(noUser))
       }
       var insertedUser:any = await updateUser(req.body.username, user[0].email, user[0].user);
-      var success = `{"status_code" : 201, "message": "User ${req.body.username} has been edited.", "affected_rows" : ${insertedUser.affectedRows}}`;
-      res.status(201) && res.send(JSON.parse(success));
+      user[0].username = req.body.username
+      var success:any = {
+        status_code: 201,
+        message: `User ${req.body.username} has been edited.`,
+        user: user[0],
+        affected_rows: insertedUser.affectedRows
+      }
+      if(req.session.passport) {
+        req.session.passport.user.provider.username = req.body.username
+      }
+      res.status(201) && res.send(success);
     break;
   }
 };
