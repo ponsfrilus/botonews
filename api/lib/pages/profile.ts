8aaf9acf98e2880117a1b913d9c73d3f85d49931
Azecko <sami.panseri@epfl.ch>
2022-01-21 07:18:27 +0100

    [feat] Added the path when rendering profile.ts
    Co-authored-by: Nicolas Borboën <ponsfrilus@users.noreply.github.com>

--- api/lib/pages/profile.ts
+++ api/lib/pages/profile.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 
 const profile = (req: any, res: Response) => {
-  res.render('profile', {user: req.session.passport.user.provider});
+  res.render('profile', {user: req.session.passport.user.provider, path: req.path});
 };
 
 export default profile;
