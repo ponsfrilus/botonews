50a8ac3a5e245472baa9696a113779f406a57f9e
Azecko <sami.panseri@epfl.ch>
2022-02-04 12:36:03 +0100

    [feat] Homepage check if user is logged in or not - Deleted public-frontend folder

--- api/lib/pages/Home.ts
+++ api/lib/pages/Home.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from 'express';
 
-const home = (req: Request, res: Response) => {
-  res.end(`
-  <html><body><h3>Botonews</h3></body></html>
-  <a href="/auth/google">Authenticate with Google</a>`
-  );
+const home = (req: any, res: Response) => {
+  if(req.user) {
+    res.render('homepage', {user: req.session.passport.user.provider} );
+  } else {
+    res.render('homepage',  {user: {}});
+  }
 };
 
 export default home;
