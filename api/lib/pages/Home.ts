7d6557293f531c47ebfc1ed4aef1854866659435
Azecko <sami.panseri@epfl.ch>
2022-02-14 16:20:06 +0100

    [feat] Homepage displays news

--- api/lib/pages/Home.ts
+++ api/lib/pages/Home.ts
@@ -1,10 +1,34 @@
 import { Request, Response } from 'express';
+import { getAllSubscriptions } from '../DB';
+import { fetchGoEpfl } from '../FetchGoEpfl';
 
-const home = (req: any, res: Response) => {
+const home = async (req: any, res: Response) => {
   if(req.user) {
-    res.render('homepage', {user: req.session.passport.user.provider} );
+    let userSubscriptions = await getAllSubscriptions(req.session.passport.user.provider.userid)
+    let splashPageSubscriptions:any = []
+    userSubscriptions.subscriptions.forEach((element:any) => {
+      if(element.support.title == "SplashPage") {
+        splashPageSubscriptions.push(element)
+      }
+    });
+    let news:any = [];
+    for(let element of splashPageSubscriptions[0].sources) {
+      switch(element.title) {
+        case "Go":
+          console.log("goEPFL detected")
+          let goEpfl: BotonewsItem[] = await fetchGoEpfl({number: 5});
+          news = news.concat(goEpfl);
+        break;
+      }
+    }
+    res.render('homepage', {user: req.session.passport.user.provider, subscriptions: userSubscriptions, news: news} );
   } else {
-    res.render('homepage',  {user: {}});
+    let news:any = [];
+
+    let goEpfl: BotonewsItem[] = await fetchGoEpfl({number: 5});
+    news = news.concat(goEpfl);
+
+    res.render('homepage',  {user: {}, subscriptions: {}, news: news});
   }
 };
 
