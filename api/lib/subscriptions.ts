ed41b34e95d83bdeff34f76e9973ff41b605de91
Azecko <sami.panseri@epfl.ch>
2022-03-01 15:47:44 +0100

    [feat] Random, number & supports modalities
    - We can now choose the number and the randomization of news we want on the homepage and the settings page
    - Supports now have some modalities that affects the different options

--- api/lib/subscriptions.ts
+++ api/lib/subscriptions.ts
@@ -1,5 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 import { getSubscription, getAllSubscriptions, insertSubscription, insertSubscriptionSources, deleteSubscription, updateSubscription } from './DB';
+const validateModalities = (modalities:any) => {
+    /* TODO : Validate modalities
+        No date and time if mode is auto (when support modalities = time)
+    */
+    return modalities;
+}
+const checkIfSubAlreadyExists = (subscription:any, supportId:string) => {
+    if(subscription.support.is_unique == 1) {
+        if(subscription.support.id.toString() == supportId) {
+            return true
+        }
+    }
+    return false
+}
 const subscriptions = async (req: Request, res: Response, next: NextFunction) => {
     switch (req.method) {
         case "GET":
@@ -25,30 +39,28 @@ const subscriptions = async (req: Request, res: Response, next: NextFunction) =>
                 status_code: "400",
                 error: "Please specify a user id, source(s) and a support."
             }
-            if (!req.body.userId || !req.body.source || !req.body.support) return res.status(400) && res.send(error);
+            if (!req.body.userId || !req.body.sources || !req.body.support) return res.status(400) && res.send(error);
             var modalities = req.body.modalities || {};
+            modalities = validateModalities(modalities)
             var fetchedSubscriptions:any = await getAllSubscriptions(req.body.userId);
             if(!fetchedSubscriptions.length) {
                 var insertedSubscription:any = await insertSubscription(req.body.userId, req.body.support, modalities);
-                req.body.source.forEach(async (element: any) => {
+                req.body.sources.forEach(async (element: any) => {
                     var insertedSubscriptionSources:any = await insertSubscriptionSources(insertedSubscription.insertId, element)
                 });
                 return res.status(201) && res.send(insertedSubscription);
             }
             for (let i = 0; i < fetchedSubscriptions.subscriptions.length; i++) {
-                var subscription = fetchedSubscriptions.subscriptions[i]
-                if(subscription.support.is_unique == 1) {
-                    if(subscription.support.id.toString() == req.body.support) {
-                        var uniqueSubError = {
-                            status_code: "409",
-                            error: "You already have a subscription with this support created."
-                        }
-                        return res.status(409) && res.send(uniqueSubError);
+                if(checkIfSubAlreadyExists(fetchedSubscriptions.subscriptions[i], req.body.support)) {
+                    var uniqueSubError = {
+                        status_code: "409",
+                        error: "You already have a subscription with this support created."
                     }
+                    return res.status(409) && res.send(uniqueSubError);
                 }
             }
             var insertedSubscription:any = await insertSubscription(req.body.userId, req.body.support, modalities);
-            req.body.source.forEach(async (element: any) => {
+            req.body.sources.forEach(async (element: any) => {
                 var insertedSubscriptionSources:any = await insertSubscriptionSources(insertedSubscription.insertId, element)
             });
             res.status(201) && res.send(insertedSubscription);
@@ -70,10 +82,25 @@ const subscriptions = async (req: Request, res: Response, next: NextFunction) =>
                 status_code: "400",
                 error: "Please specify a user id, subscription id, source(s) and a support."
             }
-            if (!req.body.userId || !req.params.id_subscription || !req.body.source || !req.body.support) return res.status(400) && res.send(error);
+            if (!req.body.userId || !req.params.id_subscription || !req.body.sources || !req.body.support) return res.status(400) && res.send(error);
+            var fetchedSubscriptions:any = await getAllSubscriptions(req.body.userId);
+            // Vérifier que le support de fetchSubscriptions est similaire au support qui est puté, et si c'est le cas, ça veut dire que l'utilisateur n'a pas changé le support dans l'édition, et vu qu'elle ne l'a pas changé c'est bon
+            for (let i = 0; i < fetchedSubscriptions.subscriptions.length; i++) {
+                if(checkIfSubAlreadyExists(fetchedSubscriptions.subscriptions[i], req.body.support)) {
+                    var uniqueSubError = {
+                        status_code: "409",
+                        error: "You already have a subscription with this support created."
+                    }
+                    if(fetchedSubscriptions.subscriptions[i].subscription == req.params.id_subscription) {
+                        break;
+                    }
+                    return res.status(409) && res.send(uniqueSubError);
+                }
+            }
             var modalities = req.body.modalities || {};
+            modalities = validateModalities(modalities)
             var subscriptionId:number = parseInt(req.params.id_subscription);
-            var fetchedUpdateSubscription:any = await updateSubscription(subscriptionId, req.body.userId, req.body.support, modalities, req.body.source)
+            var fetchedUpdateSubscription:any = await updateSubscription(subscriptionId, req.body.userId, req.body.support, modalities, req.body.sources)
             res.status(201) && res.send(fetchedUpdateSubscription)
         break;
     };
